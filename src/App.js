50ff1bb02d2480ed52b6f2bdb87b9ab425426748
Eric Wajnrajch <ericwaje03@gmail.com>
2022-10-28 11:52:46 -0300

    service workers

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Corporis iusto, dignissimos quo minus similique labore quam
+                    officiis eligendi maiores ea distinctio quaerat aut odio
+                    repellat et tempore illum voluptas quia!
+                </p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
